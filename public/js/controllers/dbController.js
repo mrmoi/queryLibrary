2078906f6991c8be3a6675f4d32db64d2efe5dda
mrmoi <mlmart16@asu.edu>
2016-11-08 13:04:11 -0700

    db connection

--- public/js/controllers/dbController.js
+++ public/js/controllers/dbController.js
@@ -1,18 +1,22 @@
 /**
  * Created by moimartz on 11/8/16.
  */
+
 myApp.controller('dbController',['$scope','$rootScope',
     function($scope, $rootScope) {
-
-var firebase = require("firebase/app");
-
-require("firebase/database");
 
 // Get a reference to the database service
 var database = firebase.database();
 
-        var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
-        starCountRef.on('value', function(snapshot) {
-            updateStarCount(postElement, snapshot.val());
-        });
+        $scope.user = firebase;
+
+        $scope.addUser = function writeUserData(userId, name) {
+            firebase.database().ref('users/' + userId).set({
+                username: name
+                //email: email,
+                //profile_picture : imageUrl
+            });
+        }
+
+
 }]);
