0190fa0d8b7a26249a2fa983ec96ae3d102114ca
mrmoi <mlmart16@asu.edu>
2016-11-10 22:33:04 -0700

    ui router implemented

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -1,8 +1,47 @@
 
-var myApp = angular.module('myApp', ['ngRoute','firebase'])
+var myApp = angular.module('myApp', ['ui.router','firebase'])
     .constant('FIREBASE_URL', 'https://myqueries-3aa66.firebaseio.com/');
 
-myApp.config(['$routeProvider', function($routeProvider) {
+myApp.config(function ($stateProvider) {
+    var homeState = {
+            name: 'home',
+            url:  '/home',
+            //template: '<h3>hello world!</h3>'
+            templateUrl: 'public/partials/home.html'
+    }
+
+    var uploadState = {
+            name: 'upload',
+            url:  '/upload',
+            //template: '<h3>Its the UI-Router hello world app!</h3>'
+            templateUrl: 'public/partials/upload.html'
+    }
+
+    $stateProvider.state(homeState);
+    $stateProvider.state(uploadState);
+
+});
+
+
+/*
+myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url:'/home',
+            templateUrl:'public/partials/home.html',
+            controller: 'dbController'
+        })
+        .state('upload', {
+            url:'/upload',
+            templateUrl:'public/partials/upload.html',
+            controller:'dbController'
+        })
+}])
+*/
+/*
+myApp.config(['$stateProvider', function($routeProvider) {
     $routeProvider.
     when('/home', {
         templateUrl: 'public/partials/home.html',
@@ -20,3 +59,5 @@ myApp.config(['$routeProvider', function($routeProvider) {
         redirectTo: '/home'
     });
 }]);
+
+*/
