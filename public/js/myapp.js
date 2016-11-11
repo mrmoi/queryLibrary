8b68d4d743bb5f34d3772ce28b59c8216fdcda6c
mrmoi <mlmart16@asu.edu>
2016-11-10 23:39:23 -0700

    ui router implemented - 2

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -6,14 +6,14 @@ myApp.config(function ($stateProvider) {
     var homeState = {
             name: 'home',
             url:  '/home',
-            //template: '<h3>hello world!</h3>'
+            controller: 'dbController',
             templateUrl: 'public/partials/home.html'
     }
 
     var uploadState = {
             name: 'upload',
             url:  '/upload',
-            //template: '<h3>Its the UI-Router hello world app!</h3>'
+            controller: 'dbController',
             templateUrl: 'public/partials/upload.html'
     }
 
@@ -22,42 +22,3 @@ myApp.config(function ($stateProvider) {
 
 });
 
-
-/*
-myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
-
-    $stateProvider
-        .state('home', {
-            url:'/home',
-            templateUrl:'public/partials/home.html',
-            controller: 'dbController'
-        })
-        .state('upload', {
-            url:'/upload',
-            templateUrl:'public/partials/upload.html',
-            controller:'dbController'
-        })
-}])
-*/
-/*
-myApp.config(['$stateProvider', function($routeProvider) {
-    $routeProvider.
-    when('/home', {
-        templateUrl: 'public/partials/home.html',
-        controller: 'dbController'
-    }).
-    when('/upload', {
-        templateUrl: 'public/partials/upload.html',
-        controller: 'dbController'
-    }).
-    when('/other', {
-        templateUrl: 'public/partials/other.html',
-        controller: 'dbController'
-    }).
-    otherwise ({
-        redirectTo: '/home'
-    });
-}]);
-
-*/
