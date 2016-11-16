00b508ae2ba7e18cd6b50c58f799456f5731df7a
mrmoi <mlmart16@asu.edu>
2016-11-15 22:31:21 -0700

    deleted names.js, history.js

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -9,19 +9,14 @@ myApp.config(function ($stateProvider) {
         {
             name: 'view',
             url:  '/view',
-            //controller: 'dbController',
-            //templateUrl: 'public/partials/home.html'
-            //component: 'namesComponent',
             templateUrl: 'public/partials/view.html'
-            //views: 'results'
         },
 
         {
             name: 'upload',
             url: '/upload',
-            templateUrl: 'public/partials/upload.html',
-            component: 'historyComponent'
-
+            component: 'historyComponent',
+            templateUrl: 'public/partials/upload.html'
         },
 
         {
@@ -42,95 +37,11 @@ myApp.config(function ($stateProvider) {
             templateUrl: 'public/partials/view.cat3.html'
         }
 
-
-
-        /*,
-
-        {
-            name: 'home.two',
-            url: 'home.two',
-            templateUrl: 'home.two.html'
-
-        },
-
-        {
-            name: 'upload',
-            url:  '/upload',
-            controller: 'dbController',
-            templateUrl: 'public/partials/upload.html'
-        },
-
-        {
-            name: 'about',
-            url: '/about',
-            controller: 'dbController',
-            templateUrl: 'public/partials/about.html'
-        },
-
-        {
-            name: 'history',
-            url:  '/history',
-            controller: 'dbController',
-            //templateUrl: 'public/partials/home.html'
-            component: 'historyComponent',
-            templateUrl: 'public/partials/history.html'
-        }*/
-
-
     ]
 
     states.forEach(function (state) {
         $stateProvider.state(state);
     });
-
-  /*
-    var homeState = {
-            name: 'home',
-            url:  '/home',
-            //controller: 'dbController',
-            //templateUrl: 'public/partials/home.html'
-            component: 'home'
-    }
-
-    var uploadState = {
-            name: 'upload',
-            url:  '/upload',
-            controller: 'dbController',
-            templateUrl: 'public/partials/upload.html'
-    }
-
-    var names = {
-        name: 'names',
-        url:  '/names',
-        //controller: 'dbController',
-        //templateUrl: 'public/partials/upload.html'
-        component: 'names'
-    }
-/*
-    var namesState = {
-        name: 'names',
-        url:  '/home',
-        //controller: 'dbController',
-        //templateUrl: 'public/partials/upload.html'
-        template: '<h2>This is names state</h2>'
-    }
-
-    var sportsState = {
-        name: 'sports',
-        url:  '/home',
-        //controller: 'dbController',
-        //templateUrl: 'public/partials/upload.html'
-        template: '<h2>This is sports state</h2>'
-    }
-
-    $stateProvider.state(homeState);
-    $stateProvider.state(uploadState);
-    //$stateProvider.state(namesState);
-    //$stateProvider.state(sportsState);
-
-*/
-
-
 
 });
 
