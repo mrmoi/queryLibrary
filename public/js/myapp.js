19ee3af4d4791dff3f6f09f8d151c248c450d78e
mrmoi <mlmart16@asu.edu>
2016-11-18 13:31:54 -0700

    look into filter provider

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -49,5 +49,28 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state(state);
     });
 
+
+    /* FILTER FUNCTION */
+    myApp.filter('searchFor', function(){
+
+        return function(arr, searchString){
+            if(!searchString){
+                return arr;
+
+            }
+            var result = [];
+            searchString = searchString.toLowerCase();
+
+            angular.forEach(arr, function(item){
+                if(item.name.toLowerCase().indexOf(searchString) !== -1){
+                    result.push(item);
+                }
+            });
+
+            return result;
+        };
+
+    });
+
 });
 
